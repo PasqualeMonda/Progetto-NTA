d75167589b5e2f8cfc7cbc3412f7eb3cef25d7ab
NTA LAB 4 <ntalab4@Device-di-accademia.local>
2017-10-24 11:18:31 +0200

    led commentato

--- tutorial/basic/RBPi/LED.py
+++ tutorial/basic/RBPi/LED.py
@@ -1,10 +1,10 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print "LED on"
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-print "LED off"
-GPIO.output(18,GPIO.LOW)
+import RPi.GPIO as GPIO #tutti i comandi si rifanno alla libreria da noi impostata, in questo caso GPIO#
+import time #importiamo una nuova libreria che si riferisce al tempo#
+GPIO.setmode(GPIO.BCM) #senti, noi parliamo in BCM, capisc a me#
+GPIO.setwarnings(False) #non mi stampare/scrivere gli errori#
+GPIO.setup(18,GPIO.OUT) #il pin 18 sarà il nostro OUTPUT#
+print "LED on" #ci comunica l'accensione del led#
+GPIO.output(18,GPIO.HIGH) #accende il led#
+time.sleep(1) #tempo di pausa che si prende il led#
+print "LED off" #ci comunica che il led è spento#
+GPIO.output(18,GPIO.LOW) #interruzione della corrente al led#
